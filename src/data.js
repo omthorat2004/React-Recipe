d48c73b3fc67d4d9337cb36acc6ddf46b73ea496
omthorat2004 <154694985+omthorat2004@users.noreply.github.com>
2023-12-23 10:27:42 -0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,133 +1,130 @@
-export const data= [
-",
-        "200g Tofu",    {
-      id: "1",
-      title: "Veggie Stew",
-      ingredients: [
-        "1 Carrot",
-        "1 Leek
-        "300ml Veg stock"
-      ],
-      method: "1. Pre-heat the oven to 200C/3C/gas 5. Place the carrot, leek and tofu in a large bowl. Add the stock and mix well. 2. Add the rest of the ingredients and mix well. 3. Place the mixture in a large bowl and cover with a lid. 4. Place the lid on the oven and cook for 40 minutes. 5. Serve with a slaw of your choice",
-      cookingTime: "45 minutes"
-    },
-    {
-      id: "2",
-      title: "Spaghetti Carbonara",
-      ingredients: [
-        "200g spaghetti",
-        "150g pancetta",
-        "2 eggs",
-        "50g grated Parmesan cheese"
-      ],
-      method: "1. Boil spaghetti until al dente. 2. Cook pancetta in a pan until crispy. 3. Whisk eggs and Parmesan in a bowl. 4. Drain pasta, add pancetta, then egg mixture. 5. Toss until coated. 6. Serve immediately.",
-      cookingTime: "20 minutes"
-    },
-    {
-      id: "3",
-      title: "Chicken Stir-Fry",
-      ingredients: [
-        "300g chicken breast",
-        "1 bell pepper",
-        "1 onion",
-        "2 tbsp soy sauce"
-      ],
-      method: "1. Cut chicken into strips, stir-fry until cooked. 2. Add sliced bell pepper and onion. 3. Stir in soy sauce. 4. Cook for 5-7 minutes. 5. Serve hot with rice.",
-      cookingTime: "25 minutes"
-    },
-    {
-      id: "4",
-      title: "Caprese Salad",
-      ingredients: [
-        "2 large tomatoes",
-        "200g fresh mozzarella",
-        "1/4 cup fresh basil leaves",
-        "2 tbsp balsamic vinegar"
-      ],
-      method: "1. Slice tomatoes and mozzarella. 2. Arrange on a plate with basil leaves. 3. Drizzle with balsamic vinegar. 4. Sprinkle with salt and pepper. 5. Serve chilled.",
-      cookingTime: "10 minutes"
-    },
-    {
-      id: "5",
-      title: "Chocolate Chip Cookies",
-      ingredients: [
-        "1 cup butter",
-        "1 cup white sugar",
-        "1 cup brown sugar",
-        "2 eggs",
-        "2 tsp vanilla extract",
-        "3 cups all-purpose flour",
-        "1 tsp baking soda",
-        "2 cups chocolate chips"
-      ],
-      method: "1. Preheat oven to 180C/350F. 2. Cream butter and sugars. 3. Beat in eggs and vanilla. 4. Mix in flour and baking soda. 5. Stir in chocolate chips. 6. Drop spoonfuls onto a baking sheet. 7. Bake for 10-12 minutes.",
-      cookingTime: "30 minutes"
-    },
-    {
-      id: "6",
-      title: "Grilled Salmon",
-      ingredients: [
-        "4 salmon fillets",
-        "2 tbsp olive oil",
-        "Salt and pepper to taste",
-        "Lemon wedges for garnish"
-      ],
-      method: "1. Preheat grill to medium-high heat. 2. Brush salmon with olive oil, season with salt and pepper. 3. Grill for 4-5 minutes per side. 4. Serve with lemon wedges.",
-      cookingTime: "15 minutes"
-    },
-    {
-      id: "7",
-      title: "Vegetable Curry",
-      ingredients: [
-        "2 potatoes",
-        "1 onion",
-        "1 cup peas",
-        "2 tbsp curry paste",
-        "400ml coconut milk"
-      ],
-      method: "1. Cook diced potatoes and onions in a pan. 2. Add peas, curry paste, and coconut milk. 3. Simmer for 15-20 minutes. 4. Serve with rice or naan bread.",
-      cookingTime: "30 minutes"
-    },
-    {
-      id: "8",
-      title: "Caesar Salad",
-      ingredients: [
-        "1 head romaine lettuce",
-        "1/2 cup croutons",
-        "1/4 cup grated Parmesan cheese",
-        "Caesar dressing"
-      ],
-      method: "1. Wash and chop lettuce, place in a bowl. 2. Add croutons and Parmesan cheese. 3. Toss with Caesar dressing. 4. Serve chilled.",
-      cookingTime: "15 minutes"
-    },
-    {
-      id: "9",
-      title: "Beef Tacos",
-      ingredients: [
-        "500g ground beef",
-        "1 packet taco seasoning",
-        "8 taco shells",
-        "Toppings: lettuce, cheese, salsa, sour cream"
-      ],
-      method: "1. Cook beef in a pan, add taco seasoning. 2. Warm taco shells in the oven. 3. Fill shells with beef and desired toppings. 4. Serve hot.",
-      cookingTime: "20 minutes"
-    },
-    {
-      "
-  
-,
-        "1/2 cup white wine",
-        "4 cups vegetable broth",
-        "2 tbsp butteid: "10",
-      title: "Mushroom Risotto",
-      ingredients: [
-        "1 cup Arborio rice",
-        "200g mushrooms",
-        "1 onionr",
-        "1/4 cup grated Parmesan cheese"
-      ],
-      method: "1. Sauté mushrooms and onions in butter. 2. Add rice, stir until translucent. 3. Pour in white wine, cook until absorbed. 4. Gradually add vegetable broth, stirring frequently. 5. Cook until rice is creamy. 6. Stir in Parmesan cheese. 7. Serve hot.",
-      cookingTime: "35 minutes"
-    }
-  ]; id:"10",
-   
+const data = [
+  {
+    id: "1",
+    title: "Veggie Stew",
+    ingredients: [
+      "200g Tofu",
+      "1 Carrot",
+      "1 Leek",
+      "300ml Veg stock"
+    ],
+    method: "1. Pre-heat the oven to 200C/3C/gas 5. Place the carrot, leek, and tofu in a large bowl. Add the stock and mix well. 2. Add the rest of the ingredients and mix well. 3. Place the mixture in a large bowl and cover with a lid. 4. Place the lid on the oven and cook for 40 minutes. 5. Serve with a slaw of your choice",
+    cookingTime: "45 minutes"
+  },
+  {
+    id: "2",
+    title: "Spaghetti Carbonara",
+    ingredients: [
+      "200g spaghetti",
+      "150g pancetta",
+      "2 eggs",
+      "50g grated Parmesan cheese"
+    ],
+    method: "1. Boil spaghetti until al dente. 2. Cook pancetta in a pan until crispy. 3. Whisk eggs and Parmesan in a bowl. 4. Drain pasta, add pancetta, then egg mixture. 5. Toss until coated. 6. Serve immediately.",
+    cookingTime: "20 minutes"
+  },
+  {
+    id: "3",
+    title: "Chicken Stir-Fry",
+    ingredients: [
+      "300g chicken breast",
+      "1 bell pepper",
+      "1 onion",
+      "2 tbsp soy sauce"
+    ],
+    method: "1. Cut chicken into strips, stir-fry until cooked. 2. Add sliced bell pepper and onion. 3. Stir in soy sauce. 4. Cook for 5-7 minutes. 5. Serve hot with rice.",
+    cookingTime: "25 minutes"
+  },
+  {
+    id: "4",
+    title: "Caprese Salad",
+    ingredients: [
+      "2 large tomatoes",
+      "200g fresh mozzarella",
+      "1/4 cup fresh basil leaves",
+      "2 tbsp balsamic vinegar"
+    ],
+    method: "1. Slice tomatoes and mozzarella. 2. Arrange on a plate with basil leaves. 3. Drizzle with balsamic vinegar. 4. Sprinkle with salt and pepper. 5. Serve chilled.",
+    cookingTime: "10 minutes"
+  },
+  {
+    id: "5",
+    title: "Chocolate Chip Cookies",
+    ingredients: [
+      "1 cup butter",
+      "1 cup white sugar",
+      "1 cup brown sugar",
+      "2 eggs",
+      "2 tsp vanilla extract",
+      "3 cups all-purpose flour",
+      "1 tsp baking soda",
+      "2 cups chocolate chips"
+    ],
+    method: "1. Preheat oven to 180C/350F. 2. Cream butter and sugars. 3. Beat in eggs and vanilla. 4. Mix in flour and baking soda. 5. Stir in chocolate chips. 6. Drop spoonfuls onto a baking sheet. 7. Bake for 10-12 minutes.",
+    cookingTime: "30 minutes"
+  },
+  {
+    id: "6",
+    title: "Grilled Salmon",
+    ingredients: [
+      "4 salmon fillets",
+      "2 tbsp olive oil",
+      "Salt and pepper to taste",
+      "Lemon wedges for garnish"
+    ],
+    method: "1. Preheat grill to medium-high heat. 2. Brush salmon with olive oil, season with salt and pepper. 3. Grill for 4-5 minutes per side. 4. Serve with lemon wedges.",
+    cookingTime: "15 minutes"
+  },
+  {
+    id: "7",
+    title: "Vegetable Curry",
+    ingredients: [
+      "2 potatoes",
+      "1 onion",
+      "1 cup peas",
+      "2 tbsp curry paste",
+      "400ml coconut milk"
+    ],
+    method: "1. Cook diced potatoes and onions in a pan. 2. Add peas, curry paste, and coconut milk. 3. Simmer for 15-20 minutes. 4. Serve with rice or naan bread.",
+    cookingTime: "30 minutes"
+  },
+  {
+    id: "8",
+    title: "Caesar Salad",
+    ingredients: [
+      "1 head romaine lettuce",
+      "1/2 cup croutons",
+      "1/4 cup grated Parmesan cheese",
+      "Caesar dressing"
+    ],
+    method: "1. Wash and chop lettuce, place in a bowl. 2. Add croutons and Parmesan cheese. 3. Toss with Caesar dressing. 4. Serve chilled.",
+    cookingTime: "15 minutes"
+  },
+  {
+    id: "9",
+    title: "Beef Tacos",
+    ingredients: [
+      "500g ground beef",
+      "1 packet taco seasoning",
+      "8 taco shells",
+      "Toppings: lettuce, cheese, salsa, sour cream"
+    ],
+    method: "1. Cook beef in a pan, add taco seasoning. 2. Warm taco shells in the oven. 3. Fill shells with beef and desired toppings. 4. Serve hot.",
+    cookingTime: "20 minutes"
+  },
+  {
+    id: "10",
+    title: "Mushroom Risotto",
+    ingredients: [
+      "1 cup Arborio rice",
+      "200g mushrooms",
+      "1 onion",
+      "1/2 cup white wine",
+      "4 cups vegetable broth",
+      "2 tbsp butter",
+      "1/4 cup grated Parmesan cheese"
+    ],
+    method: "1. Sauté mushrooms and onions in butter. 2. Add rice, stir until translucent. 3. Pour in white wine, cook until absorbed. 4. Gradually add vegetable broth, stirring frequently. 5. Cook until rice is creamy. 6. Stir in Parmesan cheese. 7. Serve hot.",
+    cookingTime: "35 minutes"
+  }
+];
